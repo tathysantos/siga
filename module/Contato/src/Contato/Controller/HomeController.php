7076503bfd4e2fba93ef2396881431e295cbe066
tathysantos <tathysantos@new-host-2.home>
2014-07-29 17:32:21 -0300

    Teste

---
+++ module/Contato/src/Contato/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Contato\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class HomeController extends AbstractActionController {
+
+    public function indexAction() {
+        return new ViewModel();
+    }
+
+}
